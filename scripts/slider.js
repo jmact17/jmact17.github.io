f3d93d029beb14213f0d13edf21ad6664a9a52e2
Jessy Ma <jessym08@gmail.com>
2018-08-07 13:50:12 -0400

    under construction

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,20 +4,29 @@ document.addEventListener("DOMContentLoaded", function() {
     let handle = document.querySelector(".handle");
     let skew = 0;
     let delta = 0;
+    var stuck = true;
     
     //if wrapper and skewed
     if (wrapper.className.indexOf("skewed") != -1) { //if found
         skew = 1000;
     }
 
-    wrapper.addEventListener("mousemove", function(e) {
+    function mouseFollow(e) {
         delta = (e.clientX - window.innerWidth / 2) / 2;
         handle.style.left = e.clientX + delta + "px";
         topLayer.style.width = e.clientX + skew + delta + "px";
-    });
+    }
+
+    // wrapper.addEventListener("mousemove", mouseFollow);
 
     window.onclick = function() {
-        location.href = "gallery.html";
+        if (stuck == false) {
+            stuck = true;
+            wrapper.removeEventListener("mousemove", mouseFollow);
+        } else {
+            stuck = false;
+            wrapper.addEventListener("mousemove", mouseFollow);
+        }
     }
 });
 
